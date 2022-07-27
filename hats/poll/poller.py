51eb546b6b57eed70c3b6d063779bd04c83a6c64
Boden <boden.bradley@gmail.com>
2022-07-27 15:54:11 -0700

    added polling

--- hats/poll/poller.py
+++ hats/poll/poller.py
@@ -8,13 +8,16 @@ import requests
 sys.path.append("")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hats_project.settings")
 django.setup()
-from hats.api.hats_rest.models import LocationVO
+from hats_rest.models import LocationVO
 # Import models from hats_rest, here.
 # from hats_rest.models import Something
 
 def get_locations():
-    response = requests.get("http://wardrobe:8000/api/locations/")
+    response = requests.get("http://wardrobe-api:8000/api/locations/")
+    print(response)
+    print("hello")
     content = json.loads(response.content)
+    print(content)
     for location in content["locations"]:
         LocationVO.objects.update_or_create(
             import_href=location["href"],
@@ -23,6 +26,7 @@ def get_locations():
 
 def poll():
     while True:
+        print("Hat polling")
         try:
             get_locations()
         except Exception as e:
